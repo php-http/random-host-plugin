cd88228e1d2f6068a5dc952cccf01e657752e82d
David Buchmann <david@liip.ch>
2022-02-23 10:03:30 +0100

    Update SetRandomHostPlugin.php

--- src/SetRandomHostPlugin.php
+++ src/SetRandomHostPlugin.php
@@ -19,12 +19,18 @@ use function array_values;
 final class SetRandomHostPlugin implements Plugin
 {
     private UriFactoryInterface $uriFactory;
-    /** @var list<string> */
+    /**
+     * A list of hostnames / IP addresses.
+     *
+     * Only protocol, host and port are used. Paths should not be set and are ignored.
+     *
+     * @var list<string>
+     */
     private array $hosts;
     private int $currentHostIndex;
 
     /**
-     * @param array{hosts: string} $config
+     * @param array{hosts: string[]} $config
      */
     public function __construct(UriFactoryInterface $uriFactory, array $config)
     {
@@ -32,6 +38,7 @@ final class SetRandomHostPlugin implements Plugin
         $resolver = new OptionsResolver();
         $resolver->setRequired('hosts');
         $resolver->setAllowedTypes('hosts', 'string[]');
+        // TODO: we need to validate that hosts has at least 1 element, or the array_rand will produce a warning and reduce `null` which is not an `int`.
 
         $this->hosts = array_values($resolver->resolve($config)['hosts']);
         $this->currentHostIndex = array_rand($this->hosts);
@@ -39,10 +46,13 @@ final class SetRandomHostPlugin implements Plugin
 
     public function handleRequest(RequestInterface $request, callable $next, callable $first): Promise
     {
+        // would it make sense to cache this? either build all uri in the constructor, or just the one we currently use (in the happy case we will keep using the same)
+        // doing in the constructor would have the added benefit that we validate that the host names can be parsed.
         $uri = $this->uriFactory->createUri($this->hosts[$this->currentHostIndex]);
 
         $request = $request->withUri($request->getUri()
             ->withHost($uri->getHost())
+            // what happens if the uri does not specify a scheme or port?
             ->withScheme($uri->getScheme())
             ->withPort($uri->getPort()));
 
@@ -55,6 +65,7 @@ final class SetRandomHostPlugin implements Plugin
                 return $response;
             },
             function (ClientExceptionInterface $exception) {
+                // we should check for Psr\Http\Client\NetworkExceptionInterface here, to be psr-18 compliant
                 if ($exception instanceof NetworkException) {
                     $this->swapCurrentHost();
                 }
@@ -66,10 +77,10 @@ final class SetRandomHostPlugin implements Plugin
 
     private function swapCurrentHost(): void
     {
-        $oldHost = $this->hosts[$this->currentHostIndex];
-        unset($this->hosts[$this->currentHostIndex]); // Making sure it's impossible to select same host 2 times in a row
+        $previousHost = $this->hosts[$this->currentHostIndex];
+        unset($this->hosts[$this->currentHostIndex]); // Temporarily remove the previous host to be sure to select a different host
         $this->hosts = array_values($this->hosts); // Making sure $hosts is a list to avoid gaps and issues with internal array pointer
         $this->currentHostIndex = $this->hosts ? array_rand($this->hosts) : 0;
-        $this->hosts[] = $oldHost;
+        $this->hosts[] = $previousHost;
     }
 }
